8c9d02f6684f5f4a109d15cb0953e80d7b123460
ylhaven <ylhaven@foxmail.com>
2016-09-25 13:59:02 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/cdhigh/KindleEar.git
+git clone https://github.com/ylhaven/KindleEar.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
